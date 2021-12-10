1ca08f3be11a8fdddb08e7bed6f63273030c80c0
gabrielikpaetuk <gabrielikpaetuk@byui.edu>
2021-12-09 17:45:45 -0700

    Initial Commit

---
+++ src/main/java/com/example/jokeengine/JokeEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jokeengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeEngineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JokeEngineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jokeengine/JokeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokeengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeEngineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
