3f139f5340354795a2cd98daaa47ea6f2a1d8a75
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2021-12-29 20:11:26 -0700

    Refactor JokeEngineControllerTest

--- src/test/java/com/example/jokeengine/controller/JokeEngineControllerTest.java
+++ src/test/java/com/example/jokeengine/controller/JokeEngineControllerTest.java
@@ -1,28 +1,76 @@
 package com.example.jokeengine.controller;
 
+import com.example.jokeengine.client.JokeRepoClient;
+import com.example.jokeengine.model.Joke;
+import com.example.jokeengine.model.Jokes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.IntStream;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class JokeEngineControllerTest {
 
   @Autowired
-  private WebTestClient webTestClient;
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private JokeRepoClient jokeRepoClient;
+
+  @BeforeEach
+  void init() {
+    Jokes jokes  = generateJokesForTesting();
+    // Mocks the response from the external api call from jokeRepoClient.getJokes()
+    when(jokeRepoClient.getJokes()).thenReturn(jokes);
+  }
 
   @Test
-  public void test_getJokesEndpoint() {
-    webTestClient.get().uri("/get-engine-jokes?number=1")
-        .accept(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus().isOk()
-        .expectHeader().contentType(MediaType.APPLICATION_JSON);
+  public void test_getJokesEndpoint() throws Exception {
+
+    String parameterValue = "1";
+    MvcResult mvcResult = mockMvc.perform(get("/get-engine-jokes")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("number", parameterValue))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    String jokesAsStringFromMvcResult = mvcResult.getResponse().getContentAsString();
+    Jokes jokesFromMvcResult = objectMapper.readValue(jokesAsStringFromMvcResult, Jokes.class);
+
+    assertThat(jokesFromMvcResult)
+            .hasFieldOrProperty("jokes")
+            .hasNoNullFieldsOrProperties();
+
+    assertEquals(Integer.valueOf(parameterValue),
+            jokesFromMvcResult.getJokes().size());
+  }
+
+  private Jokes generateJokesForTesting() {
+    Jokes jokes = new Jokes();
+    IntStream.range(0, 15)
+            .forEach(x -> jokes.addJoke(
+                    Joke.builder()
+                            .id("JokeID-".concat(String.valueOf(x)))
+                            .content("Joke ".concat(String.valueOf(x)))
+                            .build()));
+    return jokes;
   }
 }
